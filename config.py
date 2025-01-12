1b131e84e261f62626155eadb222a9790b8da006
AmirGadami <aghad051@uottawa.ca>
2025-01-12 15:41:34 -0500

    Move dataset folder inside the clinicalai directory

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 if BASE_DIR not in sys.path:
     sys.path.append(BASE_DIR)
 
-
+MODULE_PATH = os.path.join(BASE_DIR,"clinicalai")
 DATASET_NAME = "MedicalData.json"
-RAW_DATA_DIR = os.path.join(BASE_DIR,"datasets","raw")
+RAW_DATA_DIR = os.path.join(MODULE_PATH,"datasets","raw")
 
-PROCESSES_DATA_DIR = os.path.join(BASE_DIR,"datasets","processed")
+PROCESSES_DATA_DIR = os.path.join(MODULE_PATH,"datasets","processed")
